1512df73cbeeddce6dab3da82ed11cc91568d2dc
Mehxkeleton <29153833+Mehxkeleton@users.noreply.github.com>
2019-09-19 22:30:55 -0600

    ejercicio 21
    Hacer un programa que calcule independientemente la suma de los pares y los impares de
    los números entre 1 y 1000.

---
+++ 21.js
@@ -0,0 +1,12 @@
+let sum1=0;
+let sum2=0;
+for (let i=1; i<=1000;i++){
+	console.log(i);
+	if(i%2){
+		sum1+=i;
+	}else{
+		sum2+=i;
+	}
+}
+console.log(`suma de impares ${sum1}`);
+console.log(`suma de pares ${sum2}`);
